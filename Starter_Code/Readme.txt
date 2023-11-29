b42f951e81cfc53eee9f4e95933251a03bd46557
PythonPuppy <shruti.deshpande@outlook.com>
2023-11-29 14:57:24 -0600

    first commit

---
+++ Starter_Code/Readme.txt
@@ -0,0 +1,15 @@
+Module 10 Challenge Assignment
+
+This repository contains the script and associated files to analyze data concerning weather measurements as well as report it via api
+
+Contains:
+
+Resources:
+
+hawaii_measurements.csv: csv of weather measures by date and station
+hawaii_stations.csv: csv of weather stations with associated information
+hawaii.sqlite: file to format csvs for sql
+WeatherAnalysis:
+
+climate_starter.ipynb: notebook to perform basic data analysis of sqlite files
+app.py: script to run api based on ipynb and sqlite with multiple paths to return different metrics related to the weather measurements
--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,30 +1,111 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+st = Base.classes.station
+me = Base.classes.measurement
+
+# Create session engine
+session=Session(engine)
 
 
-# Save references to each table
 
-
-# Create our session (link) from Python to the DB
-
-
-#################################################
+###############################################
 # Flask Setup
 #################################################
 
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route('/')
+def home():
+    # List all the available routes.
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation - dictionary of precipitation for one year<br/>"
+        f"/api/v1.0/stations - list of stations<br/>"
+        f"/api/v1.0/tobs - list of temperature observations for most active station<br/>"
+        f"/api/v1.0/start - min,max, and avg temperature from start date (must be in YYYY-MM-DD format)<br/>"
+        f"/api/v1.0/start/end - min,max, and avg temperature from start date to end date (must be in YYYY-MM-DD format)"
+    )
+
+
+@app.route('/api/v1.0/precipitation')
+def prcp():
+    # Calculate the date one year from the last date in data set.
+    current_date = session.query(me.date).order_by(me.date.desc()).first()[0]
+    prev_year = dt.datetime.strftime(dt.datetime.strptime(current_date,'%Y-%m-%d') - dt.timedelta(days = 365),'%Y-%m-%d')
+
+    # Perform a query to retrieve the data and precipitation scores
+    dt_prcp = session.query(me.date,me.prcp).\
+        filter(me.date >= prev_year).\
+        order_by(me.date.desc()).all()
+        
+    session.close()
+
+ # Precipitation scores and date held in dictionaries
+    precipitation_dictionary = {date: pr for (date, pr) in dt_prcp}
+    # Return dictionary jsonify
+    return jsonify(precipitation_dictionary)
+
+
+
+@app.route('/api/v1.0/stations')
+def stations():
+    # Query station id and name
+    station_list = session.query(st.station,st.name).all()
+    print('Successful stations query')
+    session.close()
+    return jsonify([(i[0],i[1]) for i in station_list])
+
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    # Calculate the date one year from the last date in data set.
+    current_date = session.query(me.date).order_by(me.date.desc()).first()[0]
+    prev_year = dt.datetime.strftime(dt.datetime.strptime(current_date,'%Y-%m-%d') - dt.timedelta(days = 365),'%Y-%m-%d')
+
+    # Order stations by number of occurrences for the active station
+    active_st=session.query(me.station,(func.count(me.id))).group_by(me.station).order_by(func.count(me.id).desc()).all()
+    
+    # Find temperature observations of most active station
+    station_name = active_st[0][0]
+    temps=session.query(me.tobs).filter(me.station == station_name).filter(me.date >= prev_year).all()
+    print('Successful tobs query')
+    session.close()
+    return jsonify([i[0] for i in temps])
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
